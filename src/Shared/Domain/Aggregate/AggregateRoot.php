a9fcfeeb7befd7757ae4e45076abb01b01d6a1de
Roymaniac <amolojoseph2020@gmail.com>
2026-08-07 21:43:32 +0100

    feat(shared): refactor aggregate root and domain events
    - added new method domain events
    - fixed entity test error by renaming `DummyId` class to `EntityDummyId`

--- src/Shared/Domain/Aggregate/AggregateRoot.php
+++ src/Shared/Domain/Aggregate/AggregateRoot.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Shared\Domain\Aggregate;
 
+use Shared\Domain\Entity\Entity;
 use Shared\Domain\Event\RecordDomainEvents;
 
-abstract class AggregateRoot
+abstract class AggregateRoot extends Entity
 {
     use RecordDomainEvents;
 }

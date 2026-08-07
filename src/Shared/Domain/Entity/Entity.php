c21e9bbc3eedfbe4de3e87b266f40e55dd942089
Roymaniac <amolojoseph2020@gmail.com>
2026-08-07 20:47:18 +0100

    feat(shared): refactor base Entity abstraction
    - introduce refactored shared value object into entity
    - added shared unit test for entity

--- src/Shared/Domain/Entity/Entity.php
+++ src/Shared/Domain/Entity/Entity.php
@@ -4,7 +4,21 @@ declare(strict_types=1);
 
 namespace Shared\Domain\Entity;
 
+use Shared\Domain\ValueObject\ValueObject;
+
 abstract class Entity
 {
-    abstract public function equals(self $other): bool;
+    /**
+     * Returns the identifier of the entity
+     */
+    abstract public function id(): ValueObject;
+
+    /**
+     * Compares two entities for equality
+     */
+    final public function equals(self $other): bool
+    {
+        return static::class === $other::class
+            && $this->id()->equals($other->id());
+    }
 }
--- src/Shared/Domain/ValueObject/ValueObject.php
+++ src/Shared/Domain/ValueObject/ValueObject.php
@@ -9,6 +9,11 @@ use JsonSerializable;
 abstract class ValueObject implements JsonSerializable
 {
 
+    /**
+     * Return the scalar representation of this value object
+     *
+     * @return array<string, mixed>
+     */
     abstract protected function toArray(): array;
 
     final public function equals(self $other): bool
@@ -17,6 +22,11 @@ abstract class ValueObject implements JsonSerializable
             && $this->toArray() === $other->toArray();
     }
 
+    /**
+     * Serialize the value object into an array
+     *
+     * @return array<string, mixed>
+     */
     final public function jsonSerialize(): array
     {
         return $this->toArray();
